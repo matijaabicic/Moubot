299d6cd7d0cd3aaff3bab499e2055e27c0da607a
Matija Abicic <matija.abicic@gmail.com>
2015-08-20 18:15:34 -0700

    added the request module and a simple test to call google homepage every second

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
-// add http module
+// add http module and client
 var http = require('http');
+var request = require('request');
 
 //let the port be configurable
 const PORT = 8080;
@@ -12,6 +13,8 @@ function handleRequest(request, response){
 
 var server = http.createServer(handleRequest);
 
+
+
 //spin up the listener
 server.listen(PORT, function(){
   //callback when server is successfully listening
@@ -19,6 +22,12 @@ server.listen(PORT, function(){
 
   //set up a timer.
   setInterval(function(){
-    console.log('test');
+
+    //console.log('test');
+    request('http://www.google.com', function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+        console.log(body); // Print the google web page.
+     }
+   });
   }, 1000);
 } );
