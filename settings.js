cb7ab80f26922a025297d20fe791e8c0b0a01ca7
Matija Abicic <matija.abicic@gmail.com>
2015-09-07 23:44:24 -0700

    Set default loop interval to 1 minute

--- settings.js
+++ settings.js
@@ -2,7 +2,7 @@
 
 module.exports = {
   serverPort : 8080,
-  pingInteralInMilliseconds : 1000*5, // server main loop interval. 6 hours
+  pingInteralInMilliseconds : 1000*60, // server main loop interval. 6 hours
   postBeforeTheMatch : true,
   postAfterTheMatch : true,
   preMatchWindowInMinutes: "in 5 minutes", // moment.js humanize expression
