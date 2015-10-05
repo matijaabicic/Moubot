f22aa5d49e55e56f8b55fac56a50edc095f75193
Matija Abicic <matija.abicic@gmail.com>
2015-10-04 21:52:03 -0700

    added another test

--- test/phrase-replacer-spec.js
+++ test/phrase-replacer-spec.js
@@ -3,10 +3,14 @@ var phraseReplacer = require("../lib/phrase-replacer.js");
 
 describe(phraseReplacer, function(){
   describe("replace", function(phrase, replacements){
-    it("should contain replace function", function(){
-      var result = phraseReplacer.replace("abc", 'abc');
-
-      expect(result).to.not.equal(null);
+    it("non-tagged phrases should return the same thing", function(){
+      var nonTaggedString = "Non-tagged string";
+      var result = phraseReplacer.replace(nonTaggedString, 'abc');
+      expect(result).to.be.equal(nonTaggedString);
+    });
+    it("null strings should return null", function(){
+      var result = phraseReplacer.replace(null, null);
+      expect(result).to.be.equal(null);
     });
   });
 });
