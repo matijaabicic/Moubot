b09d2d190e2535213927fb00798b3a7fe11ccaff
Matija Abicic <matija.abicic@gmail.com>
2015-10-04 21:41:02 -0700

    first passing test

--- test/phrase-replacer-spec.js
+++ test/phrase-replacer-spec.js
@@ -1,6 +1,12 @@
 var expect = require("chai").expect;
 var phraseReplacer = require("../lib/phrase-replacer.js");
 
-describe(replace, function(){
-  
+describe(phraseReplacer, function(){
+  describe("replace", function(phrase, replacements){
+    it("should contain replace function", function(){
+      var result = phraseReplacer.replace("abc", 'abc');
+
+      expect(result).to.not.equal(null);
+    });
+  });
 });
