308a0f48947f8fde2edd34c81ae40d16232d16b4
Matija Abicic <matija.abicic@gmail.com>
2015-10-11 13:41:59 -0700

    #24 - added a list of hosts that are to be excluded from google analytics

--- settings.js
+++ settings.js
@@ -11,5 +11,6 @@ module.exports = {
   preMatchWindowCloseTimeInMinutesBeforeMatch: 1, //time before the match in minutes when ranting closes
   postMatchWindowInHours: 2,  // time after then match in hours when we are ready to rant
   postMatchWindowCloseTimeInHoursAfterMatch: 24, // time after the match in hours when rant window closes
-  GA : 'UA-68256772-1'// Google analytics tracking ID
+  GA : 'UA-68256772-1', // Google analytics tracking ID
+  gaIgnoreHosts: ['localhost:8080', 'localhost:5000', 'moubot.com'] //list of hosts to be ignored on API paths
 };
