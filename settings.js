6e7f87b0ed00630a404829cf1352908fb29348ae
Matija Abicic <matija.abicic@gmail.com>
2016-01-18 07:19:26 -0800

    reduce time window after match to from 24 to 4 hours

--- settings.js
+++ settings.js
@@ -10,7 +10,7 @@ module.exports = {
   preMatchWindowInMinutes: 5, // time before the match in minutes when we are ready to rant
   preMatchWindowCloseTimeInMinutesBeforeMatch: 1, //time before the match in minutes when ranting closes
   postMatchWindowInHours: 2,  // time after then match in hours when we are ready to rant
-  postMatchWindowCloseTimeInHoursAfterMatch: 6, // time after the match in hours when rant window closes
+  postMatchWindowCloseTimeInHoursAfterMatch: 4, // time after the match in hours when rant window closes
   GA : 'UA-68256772-1', // Google analytics tracking ID
   gaIgnoreHosts: ['localhost:8080', 'localhost:5000', 'moubot.com'] //list of hosts to be ignored on API paths
 };
